91889fc55a8d3822adeb0215037900a3c2133b17
Dorian Reyes <doreyes@gbm.net>
2025-08-14 10:04:56 -0600

    Ajuste

--- widgets/recaptcha/RecaptchaWidget.js
+++ widgets/recaptcha/RecaptchaWidget.js
@@ -87,10 +87,8 @@ const recaptchaWidgetDefinition = {
         if ((val === "" || val == null) && initialProps.required) {
           return "Por favor, verifica el reCAPTCHA";
         }
-        // null indica que el valor es válido según la doc de Leap
         return null;
       },
-
       setProperty: (propName, propValue) => {
         if (propName === "siteKey") {
           initialProps.siteKey = propValue;
